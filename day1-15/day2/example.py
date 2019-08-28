1d15d546e8e461d46550befeefb6a1300cfba12a
witcher8848 <1821427401@qq.com>
2019-08-28 15:10:57 +0800

    1

---
+++ day1-15/day2/example.py
@@ -0,0 +1,16 @@
+'''
+input()和int（）和type使用
+'''
+# a = int(input('a= '))
+# b = int(input('b= '))
+# print('%d + %d = %d'%(a,b,a+b),type(a))
+a = 100
+b = 12.345
+c = 1 + 5j
+d = 'hello, world'
+e = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
